03aee711cd125443ab512071b90221b9a8d37977
Nico Poblete <nly_poblet@yahoo.com>
2016-09-03 18:49:43 -0700

    Add files via upload

--- src/Timer.java
+++ src/Timer.java
@@ -1,48 +1,65 @@
 import java.awt.*;
+import java.awt.image.*;
 
 public class Timer {
 
+	// TIMER SPRITE.
+	private BufferedImage _timerImg;
+	
+	// TIMER PATH.
+	private String _timerImgPath = "../img/timer.png";
+	
 	// DELAY CONSTANT.
 	private final int DELAY = 60;
 	
-	// SET TIMER COUNTS.
+	// TIMER COUNT.
 	private int _framecount;
-	private int _tick;
-	private int _second;
+	private int _timercount;
+	
+	// TIMER ACTIVE.
+	private boolean _timerOn;
 	
 	public Timer(int sec) {
+		_timerImg = ImageBank.loadImage(_timerImgPath);
+		
 		_framecount = 0;
 		
-		_second = sec;
+		_timercount = sec;
+		
+		_timerOn = false;
 	}
 	
 	public void update() {
 		_framecount++;
 		
 		if (_framecount == DELAY) {
-			_tick++;
+			_timercount--;
 			_framecount = 0;
-		}
-		
-		if (_tick == DELAY) {
-			_second--;
-			_tick = 0;
 		}
 	}
 	
 	public void draw(Graphics g) {
-		int minute = (int) (_second / DELAY);
+		int minute = (int) (_timercount / DELAY);
+		int second = (int) (_timercount % DELAY);
 		
-		// Draw the clock
-		//g.drawImage(clockImg, Game.WIDTH - 50, Game.HEIGHT - 10, clockImg.getWidth(), clockImg.getHeight());
-		g.drawString(String.format("%02d:%02d", minute, _second), Game.WIDTH - 20, Game.HEIGHT - 10);
+		// Draw the timer img and clock
+		g.drawImage(_timerImg, Game.WIDTH - 50, Game.HEIGHT - 10, _timerImg.getWidth(), _timerImg.getHeight(), null);
+		g.drawString(String.format("%02d:%02d", minute, second), Game.WIDTH - 40, Game.HEIGHT - 10);
 	}
 	
-	public void setSecond(int s) {
-		_second = s;
+	public void setTime(int s) {
+		_timercount = s;
 	}
 	
-	public int getSecond() {
-		return _second;
+	public void setTimer(boolean b) {
+		_timerOn = b;
+	}
+	
+	public int getCount() {
+		return _timercount;
+	}
+	
+	public boolean isActive() {
+		return _timerOn;
 	}
 }
