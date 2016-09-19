e411e859a890808d3c7040de02d868e7f6e73f5c
Nico Poblete <nly_poblet@yahoo.com>
2016-09-18 18:16:25 -0700

    Added more methods to camera
    I still haven't implemented the functionality of the camera, just the boundaries.

--- src/Camera.java
+++ src/Camera.java
@@ -1,20 +1,66 @@
 
 public class Camera {
 	
-	// OFFSET.
-	private final int _OFFSET = 10;
+	// CONSTANTS.
+	private final int _OFFSET = 2;
 	
 	// COORDINATES.
 	private int _x;
 	private int _y;
 	
+	// THRESHOLDS.
+	private int _xmin;
+	private int _xmax;
+	private int _ymin;
+	private int _ymax;
+	
+	public Camera() {
+		_xmin = 0;
+		_xmax = _OFFSET;
+		_ymin = 0;
+		_ymax = _OFFSET;
+	}
+	
 	public Camera(int x, int y) {
-		_x = x;
-		_y = y;
+		_xmin = 0;
+		_xmax = _OFFSET;
+		_ymin = 0;
+		_ymax = _OFFSET;
+		
+		setLocation(x,y);
+	}
+	
+	public void update() {
+		
 	}
 	
 	public void setLocation(int x, int y) {
 		_x = x;
 		_y = y;
+		
+		setBounds();
+	}
+	
+	public void setBounds() {
+		if (_x < _xmin) {
+			_x = _xmin;
+		}
+		if (_x > _xmax) {
+			_x = _xmax;
+		}
+		if (_y < _ymin) {
+			_y = _ymin;
+		}
+		if (_y > _ymax) {
+			_y = _ymax;
+		}
+	}
+	
+	public int getx() {
+		return _x;
+	}
+	
+	public int gety() {
+		return _y;
 	}
 }
