485245aec4d4464277e2de4531ba716bc4af573c
Nico Poblete <nly_poblet@yahoo.com>
2016-10-04 22:31:46 -0700

    Updated constructors

--- src/BronzeCoin.java
+++ src/BronzeCoin.java
@@ -1,12 +1,12 @@
 
 public class BronzeCoin extends Money {
 	
-	public BronzeCoin() {		
+	public BronzeCoin() {
+		super(10);
+		
 		_img = Constants._bronzeCoin;
 		
 		_width = _img.getWidth();
 		_height = _img.getHeight();
-		
-		setValue(10);
 	}
 }
