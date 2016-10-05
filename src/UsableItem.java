7bb35697ea6aee24ca25e5eb22f6bfd5a532fcda
Nico Poblete <nly_poblet@yahoo.com>
2016-10-04 22:24:26 -0700

    Deleted redundant abstract methods

--- src/UsableItem.java
+++ src/UsableItem.java
@@ -1,12 +1,5 @@
-import java.awt.*;
 
 public abstract class UsableItem extends Item {
 
-	@Override
-	public abstract void update();
-
-	@Override
-	public abstract void draw(Graphics g);
-
-	public abstract void effect();
+	public abstract void use(Player p);
 }
