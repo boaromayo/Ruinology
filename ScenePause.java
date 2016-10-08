5b3d6b8082459fba26d0bbc07ac310cafa3c1c3c
Nico Poblete <nlypoblet@gmail.com>
2016-10-07 18:59:31 -0700

    Polished pause scene

---
+++ ScenePause.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import java.awt.image.*;
+
+public class ScenePause extends Scene {
+
+	// PAUSE TITLE.
+	private BufferedImage _pauseImg;
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		_pauseImg = Constants._pauseTitle;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		// Press 'ENTER' to resume, 'ESC' to go to menu.
+		if (InputBank.keyDown(InputBank._ENTER)) {
+			SceneBank.removeScene();
+			SceneBank.setScene(SceneBank.getCurrentScene());
+		} else if (InputBank.keyDown(InputBank._ESC)) {
+			SceneBank.clear();
+			SceneBank.setScene(new SceneMenu());
+		}
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		drawPause(g);
+	}
+
+	private void drawPause(Graphics g) {
+		g.drawImage(_pauseImg, 20, 150, _pauseImg.getWidth(), _pauseImg.getHeight(), null);
+	}
+}
