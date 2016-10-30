9cc16bd2c5a1663a44da5858d16b414f7ab09d42
Nico Poblete <nlypoblet@gmail.com>
2016-10-30 00:24:13 -0700

    Reverted changes

--- src/InputBank.java
+++ src/InputBank.java
@@ -5,16 +5,18 @@ public class InputBank {
 	// CURRENT KEYS.
 	private static final int _TOTALKEYS = 10;
 	
-	public static final int _W = 0;
-	public static final int _A = 1;
-	public static final int _S = 2;
-	public static final int _D = 3;
-	public static final int _UP = 4;
-	public static final int _LEFT = 5;
-	public static final int _RIGHT = 6;
-	public static final int _DOWN = 7;
-	public static final int _ENTER = 8;
-	public static final int _ESC = 9;
+	public static final int _W = KeyEvent.VK_W;
+	public static final int _A = KeyEvent.VK_A;
+	public static final int _S = KeyEvent.VK_S;
+	public static final int _D = KeyEvent.VK_D;
+	public static final int _UP = KeyEvent.VK_UP;
+	public static final int _LEFT = KeyEvent.VK_LEFT;
+	public static final int _RIGHT = KeyEvent.VK_RIGHT;
+	public static final int _DOWN = KeyEvent.VK_DOWN;
+	public static final int _ENTER = KeyEvent.VK_ENTER;
+	public static final int _ESC = KeyEvent.VK_ESCAPE;
+	
+	private static int [] _keys = { _W, _A, _S, _D, _UP, _LEFT, _RIGHT, _DOWN, _ENTER, _ESC };
 	
 	// KEY PRESS MEMORY STORAGE.
 	private static boolean [] _keyBefore = new boolean[_TOTALKEYS];
@@ -23,39 +25,8 @@ public class InputBank {
 	// CHECK FOR KEYS.
 	private static int checkKey(int key) {
 		int newKey = 0;
-		switch(key) {
-		case KeyEvent.VK_W:
-			newKey = _W;
-			break;
-		case KeyEvent.VK_A:
-			newKey = _A;
-			break;
-		case KeyEvent.VK_S:
-			newKey = _S;
-			break;
-		case KeyEvent.VK_D:
-			newKey = _D;
-			break;
-		case KeyEvent.VK_UP:
-			newKey = _UP;
-			break;
-		case KeyEvent.VK_LEFT:
-			newKey = _LEFT;
-			break;
-		case KeyEvent.VK_RIGHT:
-			newKey = _RIGHT;
-			break;
-		case KeyEvent.VK_DOWN:
-			newKey = _DOWN;
-			break;
-		case KeyEvent.VK_ENTER:
-			newKey = _ENTER;
-			break;
-		case KeyEvent.VK_ESCAPE:
-			newKey = _ESC;
-			break;
-		default:
-			break;
+		for (int i = 0; i < _keys.length; i++) {
+			if (key == _keys[i]) { newKey = i; break; }
 		}
 		
 		return newKey;
@@ -64,7 +35,40 @@ public class InputBank {
 	public static void setKey(int key, boolean set) {
 		int i = checkKey(key);
 		
-		_keyNow[i] = set;
+		switch(key) {
+		case _W:
+			_keyNow[0] = set;
+			break;
+		case _A:
+			_keyNow[1] = set;
+			break;
+		case _S:
+			_keyNow[2] = set;
+			break;
+		case _D:
+			_keyNow[3] = set;
+			break;
+		case _UP:
+			_keyNow[4] = set;
+			break;
+		case _LEFT:
+			_keyNow[5] = set;
+			break;
+		case _RIGHT:
+			_keyNow[6] = set;
+			break;
+		case _DOWN:
+			_keyNow[7] = set;
+			break;
+		case _ENTER:
+			_keyNow[8] = set;
+			break;
+		case _ESC:
+			_keyNow[9] = set;
+			break;
+		default:
+			break;
+		}
 		
 		System.out.println(i);
 	}
@@ -75,13 +79,13 @@ public class InputBank {
 		}
 	}
 	
-	public static boolean keyPressed(int i) {
-		//int i = checkKey(key);
+	public static boolean keyPressed(int key) {
+		int i = checkKey(key);
 		return _keyNow[i] && !_keyBefore[i];
 	}
 	
-	public static boolean keyDown(int i) {
-		//int i = checkKey(key);
+	public static boolean keyDown(int key) {
+		int i = checkKey(key);
 		return _keyNow[i];
 	}
 }
