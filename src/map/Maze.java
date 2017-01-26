75732d9870797ad13c0255d19fae1fabb4402d9c
Nico Poblete <nlypoblet@gmail.com>
2017-01-25 22:45:25 -0800

    Added a few changes

--- src/map/Maze.java
+++ src/map/Maze.java
@@ -68,6 +68,8 @@ public class Maze {
 		
 		_roomWidth = _rooms[0][0].getCols();
 		_roomHeight = _rooms[0][0].getRows();
+		
+		_currentRoom = _rooms[0][0];
 	}
 	
 	public void loadRooms(Room[][] newRooms) {
@@ -159,7 +161,7 @@ public class Maze {
 		_currentRoom = _rooms[randRow][randCol];
 		
 		// If the picked Room has a ladder, move player to another Room.
-		if (_rooms[randRow][randCol].getTile(centx, centy).isType("ladder") &&
+		if (_currentRoom.getTile(centx, centy).isType("ladder") &&
 				_mazeSize > 1) {
 			randRow = rand.nextInt(_mazeSize);
 			randCol = rand.nextInt(_mazeSize);
@@ -169,7 +171,7 @@ public class Maze {
 		}
 		
 		// Check if tile is solid, or impassable. If solid, move player to a passable tile.
-		if (_rooms[randRow][randCol].getTile(x,y).isSolid()) {
+		if (_currentRoom.getTile(x,y).isSolid()) {
 			x = rand.nextInt(_roomWidth);
 			y = rand.nextInt(_roomHeight);
 		}
